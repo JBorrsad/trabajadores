20b8643eea976135212076b4bb771cc9dad0e50a
JBorrsad <149406696+JBorrsad@users.noreply.github.com>
2024-05-19 23:33:18 +0200

    Add files via upload

---
+++ TrabajoIndividual_JBS/src/main/java/com/example/trabajadoresJBS/ListaTrabajadores.java
@@ -0,0 +1,12 @@
+package com.example.trabajadoresJBS;
+
+import java.util.ArrayList;
+
+public class ListaTrabajadores {
+    private final ArrayList<Trabajador> ListaTrabajadores;
+
+    public ListaTrabajadores() {
+        this.ListaTrabajadores = new ArrayList<>();
+
+    }
+}
